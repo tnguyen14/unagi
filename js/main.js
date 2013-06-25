10f43c7c4bab7970cf25c6b99d0beaeea425b639
Tri Nguyen <tri@tridnguyen.com>
2013-06-25 13:44:51 -0400

    add slickgrid

--- js/main.js
+++ js/main.js
@@ -6,7 +6,14 @@ requirejs.config({
 		dataset: 'miso.dataset/dist/miso.ds.deps.0.4.1',
 		parcoords: 'parallel-coordinates/d3.parcoords',
 		// console log wrapper - read http://patik.com/blog/detailed-console-logging/
-		log: 'console.log-wrapper/consolelog'
+		log: 'console.log-wrapper/consolelog',
+		jquery: 'jquery/jquery.min',
+		jqueryMigrate: 'jquery/jquery-migrate.min',
+		jqueryDrag: 'slickgrid/lib/jquery.event.drag-2.0.min',
+		slick: 'slickgrid/slick.core',
+		slickDataview: 'slickgrid/slick.dataview',
+		slickGrid: 'slickgrid/slick.grid',
+		slickPager: 'slickgrid/controls/slick.pager'
 	},
 	shim: {
 		"d3": {
@@ -22,20 +29,43 @@ requirejs.config({
 		},
 		"log": {
 			exports: "log"
+		},
+		"jquery": {
+			exports: "jQuery"
+		},
+		"jqueryDrag": {
+			deps: ["jquery"]
+		},
+		"jqueryMigrate": {
+			deps: ["jquery"]
+		},
+		"slick": {
+			deps: ["jquery", "jqueryMigrate", "jqueryDrag"],
+			exports: "Slick"
+		},
+		"slickDataview": {
+			deps: ["jquery", "slick"]
+		},
+		"slickPager": {
+			deps: ["jquery", "slick"]
+		},
+		"slickGrid": {
+			deps: ["jquery", "slick"]
 		}
 	}
 });
 
-require(['d3', 'dataset', 'parcoords', 'log'], function(d3, dataset, parcoords, log){
+require(['d3', 'dataset', 'parcoords', 'log', "jquery", "slick", "slickGrid", "slickDataview", "slickPager"], function(d3, dataset, parcoords, log){
 
 	console = window.console || (console = { log: function(){} });
 	// get data
 	var derived,
 		dataURI = "data/world_databank_health_indicators.csv",
+		/*
 		data = new Miso.Dataset({
 			url: dataURI,
 			delimiter: ","
-		}),
+		}),*/
 		// initialize `parcoords`
 		parcoords = d3.parcoords()("#health-viz")
 			.alpha(0.4)
@@ -43,6 +73,7 @@ require(['d3', 'dataset', 'parcoords', 'log'], function(d3, dataset, parcoords, 
 			.margin({top: 50, right: 10, bottom: 50, left: 10});
 
 	// get data using Miso dataset
+	/*
 	data.fetch({
 		success: function() {
 			derived = data.countBy("Indicator Name", ["Country Name", "2011"]);
@@ -53,19 +84,109 @@ require(['d3', 'dataset', 'parcoords', 'log'], function(d3, dataset, parcoords, 
 			log('Failed to load data');
 		}
 	});
+*/
 
 	// get data using d3
 	var d3data = d3.csv(dataURI, function(data){
 		var processedData = rearrangeIndicatorData(data);
 		log(processedData);
+
+		// slickgrid needs each data element to have an id
+		processedData.forEach(function(d,i) { d.id = d.id || i; });
+
 		parcoords
 			.data(processedData)
 			.mode("queue")
 			.render()
 			.createAxes()
+			.reorderable()
 			.brushable();
-	});
 
+		/* Slick grid
+		Copied from https://github.com/syntagmatic/parallel-coordinates/blob/master/examples/slickgrid.html
+		*/
+		// setting up grid
+		var column_keys = d3.keys(processedData[0]);
+		var columns = column_keys.map(function(key,i) {
+			return {
+				id: key,
+				name: key,
+				field: key,
+				sortable: true
+			}
+		});
+
+		var options = {
+			enableCellNavigation: true,
+			enableColumnReorder: false,
+			multiColumnSort: false
+		};
+
+		var dataView = new Slick.Data.DataView();
+		var grid = new Slick.Grid("#grid", dataView, columns, options);
+		var pager = new Slick.Controls.Pager(dataView, grid, $("#pager"));
+
+		dataView.setPagingOptions({pageSize: 50});
+
+		// wire up model events to drive the grid
+		dataView.onRowCountChanged.subscribe(function (e, args) {
+			grid.updateRowCount();
+			grid.render();
+	 	});
+
+		dataView.onRowsChanged.subscribe(function (e, args) {
+			grid.invalidateRows(args.rows);
+			grid.render();
+		});
+
+		// column sorting
+		var sortcol = column_keys[0];
+		var sortdir = 1;
+
+		function comparer(a, b) {
+			var x = a[sortcol], y = b[sortcol];
+			return (x == y ? 0 : (x > y ? 1 : -1));
+		}
+
+		// click header to sort grid column
+		grid.onSort.subscribe(function (e, args) {
+			sortdir = args.sortAsc ? 1 : -1;
+			sortcol = args.sortCol.field;
+
+			if ($.browser.msie && $.browser.version <= 8) {
+				dataView.fastSort(sortcol, args.sortAsc);
+			} else {
+				dataView.sort(comparer, args.sortAsc);
+			}
+		});
+
+		// highlight row in chart
+		grid.onMouseEnter.subscribe(function(e,args) {
+			var i = grid.getCellFromEvent(e).row;
+			var d = parcoords.brushed() || data;
+			parcoords.highlight([d[i]]);
+		});
+		grid.onMouseLeave.subscribe(function(e,args) {
+			parcoords.unhighlight();
+		});
+
+		// fill grid with data
+		gridUpdate(processedData);
+
+		// update grid on brush
+		parcoords.on("brush", function(d) {
+			gridUpdate(d);
+		});
+
+		function gridUpdate(data) {
+			dataView.beginUpdate();
+			dataView.setItems(data);
+			dataView.endUpdate();
+		};
+		/*
+		End Slickgrid
+		*/
+	});
 	/*
 	Rearrange data
 
@@ -102,12 +223,13 @@ require(['d3', 'dataset', 'parcoords', 'log'], function(d3, dataset, parcoords, 
 				// loop through each indicator
 				for (var k = 0; k < numIndicators; k++) {
 					rowIndex = i + (k * totalCountries);
-					indicator = data[rowIndex]["Indicator Code"];
+					indicator = data[rowIndex]["Indicator Name"];
 					row[indicator] = data[rowIndex][years[j]];
 				}
 				d.push(row);
 			}
 		}
+		window.data = d;
 		return d;
 	}
 });
